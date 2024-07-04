1205ec4890aaffe09714c3e14bde4f8217868bf6
jeffery959 <aaronjeffery529@gmail.com>
2024-07-04 17:08:31 -0400

    another bug fixed

--- src/app/(Navigations)/Recipe/page.tsx
+++ src/app/(Navigations)/Recipe/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import "./recipe.css"
 import SingleRecipe from '@/app/Components/SingleRecipe'
 import { useGlobalContext } from '@/app/Context/store'
@@ -10,17 +10,54 @@ import { useRouter } from 'next/navigation';
 import { getRecipe } from "../../Db/Utils"
 
 const Recipe = () => {
-  const router = useRouter()
-  const searchParams = useSearchParams()
-  const pathname = usePathname()
-  const category :string|null = searchParams.get('category')
-  const Search:any = searchParams.get('search')
+ 
+
   const {recipeList,setRecipeList}:any=useGlobalContext()
-  const [srcValue,setSrcValue]=useState(Search)
   const [recipes,setRecipes]=useState(recipeList)
-  const [Categories,setCategories]=useState([{name:'All',active:false},{name:'Breakfast',active:false},{name:'Lunch',active:false},{name:'Supper',active:false},{name:'Drinks',active:false},{name:'Pasteries',active:false},{name:'Dessert',active:false}])
+
  const [RecipeTitle,setRecipeTitle]=useState("Recipe")
 
+
+  useEffect(()=>{
+getRecipe(setRecipeList)
+  },[])
+  
+
+
+  return (
+    <div className='w-full 2xl:px-64 px-2 sm:px-8 md:px-8 lg:px-24 pt-32 '>
+      <h2 className='text-Gray-Bold font-bold text-2xl w-full text-center mb-10'>{RecipeTitle}</h2>
+      <div className='w-full Recipe_Grid_1'>
+        <Suspense>
+        <NewSearch recipeList={recipeList} setRecipeTitle={setRecipeTitle} setRecipes={setRecipes}/>
+        </Suspense>
+        <div className='w-full   Recipe_Grid_1_subgrid gap-2'>
+          {
+            recipes?.map((itm:any,key:any)=>  {
+              const {UserInfo,ImageUrl,timeStamp,PostedBy,Color,_id}:any=itm
+              const newDate = new Date(timeStamp)
+ 
+              const ShrtName=PostedBy.FirstName.split('')[0]+PostedBy.LastName.split('')[0]
+             return <SingleRecipe Id={_id} PostedBy={PostedBy} Color={Color} optional_cls={false} ShrtName={ShrtName}  key={key} title={UserInfo?.Title} img={ImageUrl} postSince={newDate}/>
+           }    )
+          }
+ 
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+const NewSearch = ({recipeList,setRecipes,setRecipeTitle}:{recipeList:any,setRecipes:any,setRecipeTitle:any})=>{
+  const router = useRouter()
+  
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const [Categories,setCategories]=useState([{name:'All',active:false},{name:'Breakfast',active:false},{name:'Lunch',active:false},{name:'Supper',active:false},{name:'Drinks',active:false},{name:'Pasteries',active:false},{name:'Dessert',active:false}])
+  const category :string|null = searchParams.get('category')
+  const Search:any = searchParams.get('search')
+  const [srcValue,setSrcValue]=useState(Search)
   const FilterCategory=async(name:string)=>{
  
     
@@ -53,9 +90,6 @@ CategoryRecipe = recipeList
 
   }
   useEffect(()=>{
-getRecipe(setRecipeList)
-  },[])
-  useEffect(()=>{
     if(Search===''||Search==null){
       FilterCategory(category!)
       return
@@ -66,13 +100,8 @@ getRecipe(setRecipeList)
     }
    
   },[recipeList])
-
-
   return (
-    <div className='w-full 2xl:px-64 px-2 sm:px-8 md:px-8 lg:px-24 pt-32 '>
-      <h2 className='text-Gray-Bold font-bold text-2xl w-full text-center mb-10'>{RecipeTitle}</h2>
-      <div className='w-full Recipe_Grid_1'>
-        <div className='Recipe_Grid_1_Category w-full'>
+    <div className='Recipe_Grid_1_Category w-full'>
           <p className='hidden md:block text-Gray-Bold w-full font-bold '>Category</p>
           <input value={srcValue} onChange={(e)=>FilterSearch(e.target.value)} type="text" placeholder='Search' className='w-full py-2 px-4 border border-gray-400 outline-none foucs:border-2 focus:border-primary rounded-lg my-4' />
           <div className='flex w-full flex-wrap gap-2 justify-center md:justify-start mb-10'>
@@ -84,24 +113,8 @@ getRecipe(setRecipeList)
           </div>
 
         </div>
-        <div className='w-full   Recipe_Grid_1_subgrid gap-2'>
-          {
-            recipes?.map((itm:any,key:any)=>  {
-              const {UserInfo,ImageUrl,timeStamp,PostedBy,Color,_id}:any=itm
-              const newDate = new Date(timeStamp)
- 
-              const ShrtName=PostedBy.FirstName.split('')[0]+PostedBy.LastName.split('')[0]
-             return <SingleRecipe Id={_id} PostedBy={PostedBy} Color={Color} optional_cls={false} ShrtName={ShrtName}  key={key} title={UserInfo?.Title} img={ImageUrl} postSince={newDate}/>
-           }    )
-          }
- 
-        </div>
-
-      </div>
-    </div>
   )
 }
-
 const Chips=({text,FilterCategory}:{text:string,FilterCategory:(name:string)=>void})=>{
   
   return( <button onClick={()=>FilterCategory(text)} className='py-2 duration-200 px-4  hover:shadow-lg border-primary text-primary border rounded-md'>{text}</button >
